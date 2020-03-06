885e555153df2bf78ff2990d495bb8826734e5ae
defBig <pedrocga1@gmail.com>
2020-03-06 19:06:47 -0300

    Corrige identação em getLegalPawn

--- AI/src/Rules/Controller.java
+++ AI/src/Rules/Controller.java
@@ -38,35 +38,30 @@ public class Controller {
   	    movesP.add(x);
   	}
      }
-  	if (b.getPiece(i, j) == 'p') {
+     if (b.getPiece(i, j) == 'p') {
 
-  		if (b.getPiece(i-1, j+1) != 'o') {
-  		  x = new Coordinate(i, j, (i-1), (j+1));
-  		  movesP.add(x);
-  		  }
-
-  		if (b.getPiece(i-1, j-1) != 'o' ) {
-  		  x = new Coordinate(i, j, (i-1), (j-1));
-  		  movesP.add(x);
-  		}
-
-  		if (b.getPiece(i-1, j) == 'o') {
-  		  x = new Coordinate(i, j, (i-1), (j));
-  		  movesP.add(x);
-  		}
-  		if (b.hasPawnMoved(i, j) == false && b.getPiece(i-2, j) == 'o') {
-  	          x = new Coordinate(i, j, (i-2), (j));
-  	          movesP.add(x);
-  	        }
-  	      }
-  	   else {
-  	     IllegalArgumentExceptionreturn null;
-  	    }
-
-
-	   return movesP;
+	if (b.getPiece(i-1, j+1) != 'o') {
+	    x = new Coordinate(i, j, (i-1), (j+1));
+  	    movesP.add(x);
 	}
 
-
-
+  	if (b.getPiece(i-1, j-1) != 'o' ) {
+  	    x = new Coordinate(i, j, (i-1), (j-1));
+  	    movesP.add(x);
+  	}
+ 
+	if (b.getPiece(i-1, j) == 'o') {
+  	    x = new Coordinate(i, j, (i-1), (j));
+  	     movesP.add(x);
+  	}
+  	if (b.hasPawnMoved(i, j) == false && b.getPiece(i-2, j) == 'o') {
+  	    x = new Coordinate(i, j, (i-2), (j));
+  	    movesP.add(x);
+        }
+     }
+     else {
+         IllegalArgumentExceptionreturn null;
+     }
+     return movesP;
+    }
 }
