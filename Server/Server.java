643201180ffe07fd8da060749154db431f6776bd
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-03-06 09:25:31 -0300

    prototipo

--- Server/Server.java
+++ Server/Server.java
@@ -1,10 +1,23 @@
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class serve {
-    public static void main(String[] args) throws Exception{
-        ServerSocket server = new ServerSocket(5000);
+public class Server {
+    
+    ServerSocket server;
+    private String moviment;
 
-        
+    public Server() throws Exception{
+        server = new ServerSocket(5000);        
     }
+
+    public void sendMoviment(){
+
+    }
+    public void setMoviment(int xo, int yo, int xf, int yf){
+
+    }
+    public void finalizeCommunication(){
+
+    }
+
 }
