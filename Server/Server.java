6ea51c3d5967bd5a6137217a298d85718dbae22e
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-03-06 09:36:44 -0300

    terminando o prototipo

--- Server/Server.java
+++ Server/Server.java
@@ -3,6 +3,8 @@ import java.net.Socket;
 
 public class Server {
     
+    //TALVEZ MUDAR O TIPO DO ENVIO(STRING -> BYTE(?))...
+
     ServerSocket server;
     private String moviment;
 
@@ -11,13 +13,16 @@ public class Server {
     }
 
     public void sendMoviment(){
-
+        Socket socket = server.accept();
+        try(PrintWriter w = new PrintWriter(socket.getOutputStream())){
+            w.println(moviment);
+        }
     }
     public void setMoviment(int xo, int yo, int xf, int yf){
-
+        moviment += String.valueOf(xo)+String.valueOf(yo)+String.valueOf(xf)+String.valueOf(yf);//O split é dado por todos os chars, pois o valor das variaveis são compreendidos entre 0-7
     }
     public void finalizeCommunication(){
-
+        server.close();
     }
 
 }
