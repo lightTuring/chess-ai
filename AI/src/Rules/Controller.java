671f6127d5eae0301645c42121e4b1e8970f6d56
defBig <pedrocga1@gmail.com>
2020-03-06 18:39:35 -0300

    Comenta e reidentiza movePawn

--- AI/src/Rules/Controller.java
+++ AI/src/Rules/Controller.java
@@ -13,31 +13,31 @@ public class Controller {
 
 
   public ArrayList<Coordinate> movePawn(byte i, byte j) throws Exception {
-	  ArrayList<Coordinate> movesP = new ArrayList<Coordinate>();
-	  Coordinate x;
-
+    ArrayList<Coordinate> movesP = new ArrayList<Coordinate>();
+    Coordinate x;
+    // Se o peão for preto:
     if (b.getPiece(i, j) == 'P') {
-
-  		  if (b.getPiece(i+1, j-1) != 'o') {
-  		    x = new Coordinate(i, j, i+1, j-1);
-  		    movesP.add(x);
-  		    }
-
-  		  if (b.getPiece(i+1, j+1) != 'o') {
-  		    x = new Coordinate(i, j, (i+1), (j+1));
-  		    movesP.add(x);
-  		  }
-
-  		  if (b.getPiece(i+1, j) == 'o') {
-  		    x = new Coordinate(i, j, (i+1), (j));
-  		    movesP.add(x);
-  		  }
-
-  		  if (b.hasPawnMoved(i, j) == false && b.getPiece(i+2, j) == 'o') {
-  		    x = new Coordinate(i, j, (i+2), (j));
-  		    movesP.add(x);
-  		  }
-  		}
+	// Se a casa na diagonal esquerda tiver alguma peça:
+	if (b.getPiece(i+1, j-1) != 'o') {
+  	    x = new Coordinate(i, j, i+1, j-1);
+  	    movesP.add(x);
+  	}
+        // Se a casa na diagonal direita tiver alguma peça:
+  	if (b.getPiece(i+1, j+1) != 'o') {
+ 	    x = new Coordinate(i, j, (i+1), (j+1));
+  	    movesP.add(x);
+ 	}
+	// Se a casa a frente estiver vazia:
+  	if (b.getPiece(i+1, j) == 'o') {
+  	    x = new Coordinate(i, j, (i+1), (j));
+  	    movesP.add(x);
+ 	}
+	// Se no primeiro movimento do peão, a casa na frente da frente estiver vazia:
+  	if (b.hasPawnMoved(i, j) == false && b.getPiece(i+2, j) == 'o') {
+  	    x = new Coordinate(i, j, (i+2), (j));
+  	    movesP.add(x);
+  	}
+     }
   	if (b.getPiece(i, j) == 'p') {
 
   		if (b.getPiece(i-1, j+1) != 'o') {
