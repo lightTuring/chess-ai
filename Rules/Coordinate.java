1ade4991b3701941129007029031ed4e5b6d72a5
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-03-06 08:08:03 -0300

    alterando um pouco...

--- Rules/Coordinate.java
+++ Rules/Coordinate.java
@@ -1,8 +1,8 @@
 public class Coordinate {
-  private byte initial_i;
-  private byte initial_j;
-  private byte final_i;
-  private byte final_j;
+  byte initial_i;
+  byte initial_j;
+  byte final_i;
+  byte final_j;
 
   public Coordinate(byte initial_i, byte initial_j, byte final_i, byte final_j){
     this.initial_i = initial_i;
