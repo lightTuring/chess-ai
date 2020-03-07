09da9b192a0e4803b436912cd791907504c04cbc
defBig <pedrocga1@gmail.com>
2020-03-07 10:20:57 -0300

    Cria InvalidPieceException (ver comentários no .java)

---
+++ AI/src/Rules/InvalidPieceException.java
@@ -0,0 +1,11 @@
+package Rules;
+
+/* Exceção para ser lançada onde antes era IllegalArgumentException,
+pois essa última é mais adequada quando há incompatibilidade de tipos
+entre os argumentos e os parâmetros de um método.
+*/
+public class InvalidPieceException extends Exception {
+    public InvalidPieceException(String message) {
+	super(message);
+    }
+}
