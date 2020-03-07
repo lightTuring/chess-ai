0b3c315a7143eeb128a9e76db568fb1c28f7d041
defBig <pedrocga1@gmail.com>
2020-03-07 10:31:43 -0300

    Corrige erro no comentário que precede a classe

--- AI/src/Rules/BoardOutOfBoundsException.java
+++ AI/src/Rules/BoardOutOfBoundsException.java
@@ -1,6 +1,6 @@
 package Rules;
 
-// Para ser usada em Board.getPiece(), no lugar de InvalidArgumentException
+// Para ser usada em Board.getPiece(), no lugar de IllegaldArgumentException
 public class BoardOutOfBoundsException extends Exception {
     public BoardOutOfBoundsException(String message) {
 	if (message.isEmpty()) {
