b1dd94877cd5fd5618a6c13342a9d50dc344a9a5
lightTuring <cloud.losing@gmail.com>
2020-03-08 15:08:34 -0300

    Corrigindo erros no movimento do peão

--- AI/src/Rules/Controller.java
+++ AI/src/Rules/Controller.java
@@ -20,44 +20,44 @@ public class Controller {
     // Se o peão for Branco:
     if (b.getPiece(i, j) == 'P') {
 	// Se a casa na diagonal esquerda tiver alguma peça:
-	if (b.getPiece(i+1, j-1) != 'o') {
+	if (b.getPiece(i-1, j-1) != 'o') {
   	    x = new Coordinate(i, j, i+1, j-1);
   	    movesP.add(x);
   	}
         // Se a casa na diagonal direita tiver alguma peça:
-  	if (b.getPiece(i+1, j+1) != 'o') {
+  	if (b.getPiece(i-1, j+1) != 'o') {
  	    x = new Coordinate(i, j, (i+1), (j+1));
   	    movesP.add(x);
  	}
 	// Se a casa a frente estiver vazia:
-  	if (b.getPiece(i+1, j) == 'o') {
-  	    x = new Coordinate(i, j, (i+1), (j));
+  	if (b.getPiece(i-1, j) == 'o') {
+  	    x = new Coordinate(i, j, (i-1), (j));
   	    movesP.add(x);
  	}
 	// Se no primeiro movimento do peão, a casa na frente da frente estiver vazia:
-  	if (b.hasPawnMoved(i, j) == false && b.getPiece(i+2, j) == 'o') {
-  	    x = new Coordinate(i, j, (i+2), (j));
+  	if (b.hasPawnMoved(i, j) == false && b.getPiece(i-2, j) == 'o') {
+  	    x = new Coordinate(i, j, (i-2), (j));
   	    movesP.add(x);
   	}
      }
      if (b.getPiece(i, j) == 'p') {
 
-	if (b.getPiece(i-1, j+1) != 'o') {
-	    x = new Coordinate(i, j, (i-1), (j+1));
+	if (b.getPiece(i+1, j+1) != 'o') {
+	    x = new Coordinate(i, j, (i+1), (j+1));
   	    movesP.add(x);
 	}
 
-  	if (b.getPiece(i-1, j-1) != 'o' ) {
-  	    x = new Coordinate(i, j, (i-1), (j-1));
+  	if (b.getPiece(i+1, j-1) != 'o' ) {
+  	    x = new Coordinate(i, j, (i+1), (j-1));
   	    movesP.add(x);
   	}
 
-	if (b.getPiece(i-1, j) == 'o') {
-  	    x = new Coordinate(i, j, (i-1), (j));
+	if (b.getPiece(i+1, j) == 'o') {
+  	    x = new Coordinate(i, j, (i+1), (j));
   	    movesP.add(x);
   	}
-  	if (b.hasPawnMoved(i, j) == false && b.getPiece(i-2, j) == 'o') {
-  	    x = new Coordinate(i, j, (i-2), (j));
+  	if (b.hasPawnMoved(i, j) == false && b.getPiece(i+2, j) == 'o') {
+  	    x = new Coordinate(i, j, (i+2), (j));
   	    movesP.add(x);
         }
      }
