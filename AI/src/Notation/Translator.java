4d65b8b7d7ea80e571c8b61c737e4eabc5d484e3
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-03-11 18:39:35 -0300

    construindo o  escorpo

--- AI/src/Notation/Translator.java
+++ AI/src/Notation/Translator.java
@@ -2,6 +2,12 @@ package Notation;
 
 public class Translator {
 	
-	
-	
+	public static String NotationChessToComputer(char pos_w, byte pos_h) {
+		return null;
+		
+	}
+	public static String NotationComputerToChess(byte pos_i, byte pos_j) {
+		return null;
+		
+	}
 }
