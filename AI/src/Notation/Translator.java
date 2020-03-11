1a7e23f7f3b152192e1bf0c8c16641d86a04a8e5
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-03-11 19:03:52 -0300

    Implementado teste

--- AI/src/Notation/Translator.java
+++ AI/src/Notation/Translator.java
@@ -5,20 +5,27 @@ import java.util.Arrays;
 public class Translator {
 	
 	private static char[] letters = {'A','B','C','D','E', 'F', 'G', 'H'};
-	private static byte[] numbers = {1,2,3,4,5,6,7,8}; 
+	private static int[] numbers = {8,7,6,5,4,3,2,1}; 
 	
-	public static int[] NotationChessToComputer(char pos_w, byte pos_h) {
+	public static int[] NotationChessToComputer(char pos_w, int pos_h) {
 		
-		int[] OrderedPair = {Arrays.binarySearch(letters, pos_w), Arrays.binarySearch(numbers, pos_h)};
+		int[] OrderedPair = {Arrays.binarySearch(letters, pos_w), numbers.length + 1 + Arrays.binarySearch(numbers, pos_h)};
 				
 		return OrderedPair;
 		
 	}
-	public static String NotationComputerToChess(byte pos_i, byte pos_j) {
+	public static String NotationComputerToChess(int i, int j) {
 		
-		String OrderedPair = letters[pos_i] + String.valueOf(numbers[pos_j]);
+		String OrderedPair = letters[i] + String.valueOf(numbers[j]);
 		
 		return OrderedPair;
 		
 	}
+	/*
+	public static void main(String[] args) {
+		System.out.println(NotationComputerToChess(0, 0));
+		int[] retorno = NotationChessToComputer('A', 8);
+		System.out.println(retorno[0]+" "+retorno[1]);
+		System.out.println(retorno[1]);
+	}*/
 }
