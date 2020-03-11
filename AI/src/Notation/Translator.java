b210c7a176b3665248170e460f2d55889e5053c2
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-03-11 18:50:51 -0300

    finalizando primeiro método

--- AI/src/Notation/Translator.java
+++ AI/src/Notation/Translator.java
@@ -1,9 +1,17 @@
 package Notation;
 
+import java.util.Arrays;
+
 public class Translator {
 	
-	public static String NotationChessToComputer(char pos_w, byte pos_h) {
-		return null;
+	private static char[] letters = {'A','B','C','D','E', 'F', 'G', 'H'};
+	private static byte[] numbers = {1,2,3,4,5,6,7,8}; 
+	
+	public static int[] NotationChessToComputer(char pos_w, byte pos_h) {
+		
+		int[] OrderedPair = {Arrays.binarySearch(letters, pos_w), Arrays.binarySearch(numbers, pos_h)};
+				
+		return OrderedPair;
 		
 	}
 	public static String NotationComputerToChess(byte pos_i, byte pos_j) {
