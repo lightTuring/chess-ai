5442ff642a762ec4f92853cd2a43ed985c4f4b71
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-03-12 18:37:26 -0300

    método construtor

--- AI/src/Algorithm/DFS.java
+++ AI/src/Algorithm/DFS.java
@@ -2,4 +2,17 @@ package Algorithm;
 
 public class DFS {
 
+	private int nodes;
+	private int branches;
+	
+	private int[][] graph = new int[nodes][branches];
+	private int[] visited = new int[graph.length];
+	
+	public DFS(int[][] graph, int nodes, int branches) {
+		this.nodes = nodes;
+		this.branches = branches;
+		
+		this.graph = graph;
+	}
+	
 }
