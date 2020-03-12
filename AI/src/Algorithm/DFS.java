62919ec60d3f157aa8b59c36b3254ed3d6876a01
SageScroll18144 <lipe.fssantos063@gmail.com>
2020-03-12 18:48:14 -0300

    trocando tipo de list e implementando um teste

--- AI/src/Algorithm/DFS.java
+++ AI/src/Algorithm/DFS.java
@@ -1,18 +1,45 @@
 package Algorithm;
 
+import java.util.ArrayList;
+
 public class DFS {
 
 	private int nodes;
-	private int branches;
 	
-	private int[][] graph = new int[nodes][branches];
+	private ArrayList<Integer>[] graph = new ArrayList[nodes];
+	
+	//private int[][] graph = new int[nodes][branches];
 	private int[] visited = new int[graph.length];
 	
-	public DFS(int[][] graph, int nodes, int branches) {
+	public DFS(ArrayList<Integer>[] graph, int nodes) {
 		this.nodes = nodes;
-		this.branches = branches;
-		
 		this.graph = graph;
+	}
+	
+	public void print() {
+		for (int i = 0; i < graph.length; i++) {
+			for (int j = 0; j < graph[i].size(); j++) {
+				System.out.print(this.graph[i].get(j));
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+		int nodes = 2;
+		ArrayList<Integer>[] graph = new ArrayList[nodes];
+		
+		for (int i = 0; i < graph.length; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		graph[0].add(1);
+		graph[1].add(2);
+		graph[1].add(2);
+		
+		
+		DFS dfs = new DFS(graph, nodes);
+		
+		dfs.print();
 	}
 	
 }
