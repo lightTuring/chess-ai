07a9d0223b2aaa50fb6520ba4936604c1c08dc32
Felipe Santos <lipe.fssantos063@gmail.com>
2020-03-15 10:54:03 -0300

    creating a function to socket IO

--- Bridge/Client.py
+++ Bridge/Client.py
@@ -1,15 +1,18 @@
 import socket
 
-s = socket.socket() 
-
 host_ip = "127.0.0.1"
 port = 5005
 
-s.connect((host_ip, port))
+def read_socket():
+    s = socket.socket() 
+    s.connect((host_ip, port))
 
-return_socket = s.recv(1024)
+    return s.recv(1024)
+
+    s.close()
+
+return_socket = read_socket()
 
 print(return_socket)
 print(type(return_socket)) 
 
-s.close() 
