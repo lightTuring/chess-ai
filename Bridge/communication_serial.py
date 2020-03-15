08a7d159cec3542a4cbad8d0ce5c291a1b9ea491
Felipe Santos <lipe.fssantos063@gmail.com>
2020-03-15 11:15:38 -0300

    communication with arduino

---
+++ Bridge/communication_serial.py
@@ -0,0 +1,15 @@
+import serial
+
+class Board:
+    def __init__(self, port_arduino, baud_rate):
+        self.port_arduino = port_arduino
+        self.baud_rate = baud_rate
+
+    def begin(self):
+        self.arduino = serial.Serial(port_arduino, baud_rate)
+    def read(self):
+        return self.arduino.readline()
+    def send(self, command):
+        self.arduino.write(command.encode())
+    def finalize(self):
+        self.arduino.close()
