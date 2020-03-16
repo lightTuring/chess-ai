123dfc629bfc62b09acde7da46204f0ba6ce4a3d
Felipe Santos <lipe.fssantos063@gmail.com>
2020-03-16 13:29:40 -0300

    finalizando a classe

--- AI/src/Algorithm/Graph.java
+++ AI/src/Algorithm/Graph.java
@@ -1,11 +1,28 @@
 package Algorithm;
 
+import java.util.ArrayList;
+
 public class Graph {
 
 	private int depth;
+	private ArrayList<Integer>[] graph;
 	
 	public Graph(int depth) {
 		this.depth = depth;
+		this.graph = new ArrayList[depth];
+		for (int i = 0; i < graph.length; i++) {
+			graph[i] = new ArrayList<Integer>();
+		}
 	}
 	
+	public void addElements(int node, int ... elements) {
+		for (int i : elements) {
+			graph[node].add(i);
+		}
+	}
+	
+	public ArrayList<Integer>[] graph(){
+		return this.graph;
+		
+	}
 }
