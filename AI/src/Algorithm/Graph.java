d8f8af2a5648302bcf811357e930b96d128f45a9
Felipe Santos <lipe.fssantos063@gmail.com>
2020-03-16 18:49:35 -0300

    colocando os 'this'

--- AI/src/Algorithm/Graph.java
+++ AI/src/Algorithm/Graph.java
@@ -11,13 +11,13 @@ public class Graph {
 		this.depth = depth;
 		this.graph = new ArrayList[depth];
 		for (int i = 0; i < graph.length; i++) {
-			graph[i] = new ArrayList<Integer>();
+			this.graph[i] = new ArrayList<Integer>();
 		}
 	}
 	
 	public void addElements(int node, int ... elements) {
 		for (int i : elements) {
-			graph[node].add(i);
+			this.graph[node].add(i);
 		}
 	}
 	
