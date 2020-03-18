83c21b97b94c7bb1b66efb786d02b60005aaaef7
Felipe Santos <lipe.fssantos063@gmail.com>
2020-03-18 12:59:35 -0300

    implementando o movimento dos motores

--- Programming_Board/stepper_motor/stepper_motor.ino
+++ Programming_Board/stepper_motor/stepper_motor.ino
@@ -1,6 +1,9 @@
 #include <Stepper.h>
 
 const int stepsPerRevolution = 100; 
+int lastHouse_x = 0;
+int lastHouse_y = 0;
+int lengthOfHouse = 3;// valor em CM
 
 Stepper motors[2] = {
   Stepper(stepsPerRevolution, 4,6,5,7),
@@ -23,6 +26,12 @@ void loop(){
 
   if(Serial.available() > 0){
     String input_movement = Serial.readString();
+    ((int)input_movement[0] - (int)'0') - lastHouse_x;
+
+    axis_xy(metricToRotation(lengthOfHouse * (((int)input_movement[0] - (int)'0') - lastHouse_x)), metricToRotation(lengthOfHouse * (((int)input_movement[1] - (int)'0') - lastHouse_y)));
+
+    lastHouse_x = ((int)input_movement[0] - (int)'0');
+    lastHouse_y = ((int)input_movement[1] - (int)'0');   
   }
-  Serial.write('0');//this value is like a finalize...
+  Serial.write(0);//this value is like a finalize...
 }
