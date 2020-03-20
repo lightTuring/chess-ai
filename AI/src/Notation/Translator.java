9add5e160bfbbd4a4c980167e5a96ac13e0a664f
Felipe Santos <lipe.fssantos063@gmail.com>
2020-03-20 14:45:06 -0300

    atualizando o tradutor

--- AI/src/Notation/Translator.java
+++ AI/src/Notation/Translator.java
@@ -6,13 +6,15 @@ public class Translator {
 	
 	private static char[] letters = {'A','B','C','D','E', 'F', 'G', 'H'};
 	private static int[] numbers = {8,7,6,5,4,3,2,1}; 
+	//private static char[] letters = {'H','G','F','E','D','C','B','A'};
+	//private static int[] numbers = {1,2,3,4,5,6,7,8}; 
 	
 	public static int[] NotationChessToComputer(char pos_w, int pos_h) {
 		
 		if((int)pos_w > 90)
 			pos_w = (char)((int)pos_w - ((int)'a' - (int)'A'));
 		
-		int[] OrderedPair = {Arrays.binarySearch(letters, pos_w), numbers.length + 1 + Arrays.binarySearch(numbers, pos_h)};
+		int[] OrderedPair = {SearchGross(numbers, pos_h), Arrays.binarySearch(letters, pos_w)};
 				
 		return OrderedPair;
 		
@@ -24,11 +26,19 @@ public class Translator {
 		return OrderedPair;
 		
 	}
-	/*
+	private static int SearchGross(int[] list, int e) {
+		int element = 0;
+		for (int i = 0; i < list.length; i++) {
+			if(e == list[i])
+				element = i;
+		}
+		return element;
+	}
 	public static void main(String[] args) {
 		System.out.println(NotationComputerToChess(0, 0));
-		int[] retorno = NotationChessToComputer('A', 8);
+		int[] retorno = NotationChessToComputer('H', 2);
 		System.out.println(retorno[0]+" "+retorno[1]);
 		System.out.println(retorno[1]);
-	}*/
+	}
+	
 }
--- AI/src/Teste/Main.java
+++ AI/src/Teste/Main.java
@@ -1,10 +1,15 @@
 package Teste;
 
+import java.util.Scanner;
+
+import Notation.Translator;
 import Rules.Board;
 
 public class Main {
     public static void main(String[] args) {
-        @SuppressWarnings("unused")
+   //     @SuppressWarnings("unused")
 		Board b = new Board();
+        b.printImage();
+       
     }
 }
