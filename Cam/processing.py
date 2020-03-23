6f992f947f54fcaa939eea2d697309e9f8597765
Felipe Santos <lipe.fssantos063@gmail.com>
2020-03-23 16:26:18 -0300

    testando canny

--- Cam/processing.py
+++ Cam/processing.py
@@ -1,16 +1,19 @@
 import cv2
 import numpy as np 
 
-img = cv2.imread("tab.jpeg")
+img = cv2.imread("tab_pecas.jpeg")
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 ret, corners = cv2.findChessboardCorners(gray, (7, 7), None)
+edges = cv2.Canny(gray, 50, 100)
 
 cv2.drawChessboardCorners(img, (7, 7), corners, ret)
 
 print(ret)
-#(frame, patternsize, Mat(centers), patternfound);
+
 cv2.imshow("Board", img)
+cv2.imshow("canny", edges)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
