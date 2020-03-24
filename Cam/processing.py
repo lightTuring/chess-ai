1d019979888f837984e2c9616786e6fc30442f78
Felipe Santos <lipe.fssantos063@gmail.com>
2020-03-24 15:48:49 -0300

    finalizando método de verificação de uma peça em alguma casa

--- Cam/processing.py
+++ Cam/processing.py
@@ -1,7 +1,19 @@
 import cv2
 import numpy as np 
 
-img = cv2.imread("tab.png")
+def ThereIsAnObject(img):
+    sum = 0
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            sum+=img[i][j]
+    return (sum != 0)
+
+def salveSubImg(x, y, xw, yh, image):
+    return image[y:yh, x:xw]
+
+
+
+img = cv2.imread("board.png")
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -9,12 +21,20 @@ ret, corners = cv2.findChessboardCorners(gray, (7, 7), None)
 edges = cv2.Canny(gray, 50, 100)
 ret_c, corners_c = cv2.findChessboardCorners(edges, (7, 7), None)
 
+r = cv2.findContours(edges,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
 cv2.drawChessboardCorners(img, (7, 7), corners, ret)
 cv2.drawChessboardCorners(edges, (7, 7), corners_c, ret_c)
 
 print(ret)
-#print(float(corners[0][0][0]))
+#print(r)
+print(corners)
+print(int(corners[8][0][0]))
+#print(ThereIsAnObject(salveSubImg(edges, int(corners[0][0][0]), int(corners[0][0][1]), int(corners[8][0][0]), int(corners[8][0][1]))))
+house = edges[int(corners[0][0][0]):int(corners[8][0][0])-1, int(corners[0][0][1])+3:int(corners[8][0][1])]
+print(ThereIsAnObject(house))
 
+cv2.imshow("House", house)
 cv2.imshow("Board", img)
 cv2.imshow("canny", edges)
 
