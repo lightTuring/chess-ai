44360aeded72f64914a3a4d3432a7599ad9e6960
Felipe Santos <lipe.fssantos063@gmail.com>
2020-04-04 11:17:50 -0300

    lançando uma exception

--- AI/src/Algorithm/RandomPlay.java
+++ AI/src/Algorithm/RandomPlay.java
@@ -9,9 +9,11 @@ public class RandomPlay {
 	private final static char[] PIECES = {'p', 't', 'c', 'b', 'k', 'q'};
 	
 	//0-> preto ; 1-> branco
-	public static char choosePiece(int color) {
+	public static char choosePiece(int color) throws Exception {
 		char c = PIECES[(int)(Math.random()*(PIECES.length))];
-		if(color == 1)
+		if(color > 1)
+			throw new Exception("The value isn't a color.");
+		else if(color == 1)
 			c = (char)((int)c - ((int)'a' - (int)'A'));
 		return c;	
 	}
