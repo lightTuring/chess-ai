8970b8a076994ac1b371090ab128d919bb6c108a
Felipe Santos <lipe.fssantos063@gmail.com>
2020-04-04 10:47:16 -0300

    sobrecarregando 2 métodos

--- AI/src/Rules/Board.java
+++ AI/src/Rules/Board.java
@@ -41,6 +41,12 @@ public class Board {
     public boolean isBlack(int pos_i, int pos_j) throws BoardOutOfBoundsException {
     	return ((int)getPiece(pos_i, pos_j) > 97);
     }
+    public boolean isWhite(char piece) throws BoardOutOfBoundsException {
+    	return ((int)piece < 90);
+    }
+    public boolean isBlack(char piece) throws BoardOutOfBoundsException {
+    	return ((int)piece > 97);
+    }
     public boolean hasSameColor(int me_i, int me_j, int that_piece_i, int that_piece_j) throws BoardOutOfBoundsException {
     	return ((isBlack(me_i, me_j) && isBlack(that_piece_i, that_piece_j))||(isWhite(me_i, me_j)&& isWhite(that_piece_i, that_piece_j)));
     }
