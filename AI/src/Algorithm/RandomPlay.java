348f3d268c70085c6cdd8a9637fbfe1db84e2138
Felipe Santos <lipe.fssantos063@gmail.com>
2020-04-04 10:45:33 -0300

    escrevendo os esqueletos dos métodos

--- AI/src/Algorithm/RandomPlay.java
+++ AI/src/Algorithm/RandomPlay.java
@@ -1,5 +1,20 @@
 package Algorithm;
 
+import java.util.*;
+
+import Rules.Coordinate;
+
 public class RandomPlay {
 
+	private final char[] PIECES = {'p', 't', 'c', 'b', 'k', 'q'};
+	
+	private static char choosePiece() {
+		return 0;	
+	}
+	private static Coordinate chooseMovement() {
+		return null;	
+	}
+	public static void doMovement() {
+		
+	}
 }
