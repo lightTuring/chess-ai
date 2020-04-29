5a080941c7fa1bbd3fc5abc8fa6e0bec43a478b2
Felipe Santos <lipe.fssantos063@gmail.com>
2020-04-29 19:08:38 -0300

    init min_max

---
+++ AI/src/Algorithm/GraphBuilder.java
@@ -0,0 +1,14 @@
+package Algorithm;
+
+public class GraphBuilder {
+
+	/*
+	 *	Modelo do Grafo construido a partir da profundidade  
+	 * 
+	 */
+	
+	public GraphBuilder(int depth) {
+		
+	}
+	
+}
---
+++ AI/src/Algorithm/MinMax.java
@@ -0,0 +1,16 @@
+package Algorithm;
+
+public class MinMax {
+
+	public MinMax() {
+		// TODO Auto-generated constructor stub
+	}
+	public void algorithm() {
+		/*
+		 * 1o -> Busca o último nó por DFS 
+		 * 2o -> Seta os pesos fazendo o caminho contrário(ou seja, retira cada elemento da Stack)
+		 * 3o -> Aplica DFS nos nós de profundidade e realiza as somatórias e retorna o melhor caminho 
+		 */
+	}
+	
+}
