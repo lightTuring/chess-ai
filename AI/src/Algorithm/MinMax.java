d2635cdb1f36496e4574da23c6fa83b7b5d4a8f7
Felipe Santos <lipe.fssantos063@gmail.com>
2020-04-29 19:09:47 -0300

    init min_max

--- AI/src/Algorithm/MinMax.java
+++ AI/src/Algorithm/MinMax.java
@@ -10,6 +10,7 @@ public class MinMax {
 		 * 1o -> Busca o último nó por DFS 
 		 * 2o -> Seta os pesos fazendo o caminho contrário(ou seja, retira cada elemento da Stack)
 		 * 3o -> Aplica DFS nos nós de profundidade e realiza as somatórias e retorna o melhor caminho 
+		 * 
 		 */
 	}
 	
