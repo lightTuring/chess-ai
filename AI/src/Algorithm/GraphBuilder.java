f29f0ad5beb80bb60e72e5e2bcfa5c4e69831236
Felipe Santos <lipe.fssantos063@gmail.com>
2020-05-25 22:34:17 -0300

    novo modelo do grafo

--- AI/src/Algorithm/GraphBuilder.java
+++ AI/src/Algorithm/GraphBuilder.java
@@ -1,14 +1,44 @@
 package Algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import Rules.Coordinate;
+
 public class GraphBuilder {
 
+	private int depth = 0;
+	private LinkedList<LinkedList<Coordinate>> graph = new LinkedList<LinkedList<Coordinate>>();
+	private static Coordinate lastMovement = null;
+	
 	/*
-	 *	Modelo do Grafo construido a partir da profundidade  
+	 * ESCOLHE A PEÇA, ADICIONA NO GRAFO E CONTA A PROFUNDIDADE 
+	 * PERCORRE UM FOR PARA TODAS AS PEÇAS JOGAREM
+	 * REPETE
 	 * 
 	 */
 	
-	public GraphBuilder(int depth) {
+	public GraphBuilder() {
 		
 	}
-	
+	public void createGraph(int node, Coordinate currentPiece, LinkedList<Coordinate> plays) {
+		graph.add(node, plays);
+		if(lastMovement != null) graph.get(node).add(0, lastMovement);
+		else lastMovement = currentPiece;;
+		depth++;
+	}
+	public int getDepth() {
+		return depth;
+	}
+	/*
+	public void addElements(int node, int ... elements) {
+		for (int i : elements) {
+			this.graphConnections[node].add(i);
+		}
+	}*/
+		
+	public static void main(String[] args) {
+		
+	}
 }
