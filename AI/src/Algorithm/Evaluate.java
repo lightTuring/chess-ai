3ea6c05c52efed9ab60115df598aa8acb7359020
lightTuring <cloud.losing@gmail.com>
2020-05-29 18:24:56 -0300

    Classe de avaliação

---
+++ AI/src/Algorithm/Evaluate.java
@@ -0,0 +1,69 @@
+package Algorithm;
+
+import java.util.LinkedList;
+
+import Rules.Board;
+import Rules.BoardOutOfBoundsException;
+import Rules.Controller;
+import Rules.Coordinate;
+
+public class Evaluate {
+    double pValue = 0;
+    Board board;
+
+    public Evaluate (Board board) {
+        this.board = board;
+    }
+
+    public double piece() throws BoardOutOfBoundsException {
+        double white = 0;
+        double black = 0;
+        for (int i = 0; i<8; i++) {
+            for (int j = 0; j<8; j++) {
+                if (board.isWhite(i, j)) {
+                    if (board.getPiece(i, j) == 'P') 
+                        white = white + 1;
+                    if (board.getPiece(i, j) == 'Q')  
+                        white = white + 9;
+                    if (board.getPiece(i, j) == 'T')  
+                        white = white + 5;
+                    if (board.getPiece(i, j) == 'B')  
+                        white = white + 3.5;
+                    if (board.getPiece(i, j) == 'C')  
+                        white = white + 3;
+                    }
+                else {
+                    if (board.getPiece(i, j) == 'p') 
+                        black = black + 1;
+                    if (board.getPiece(i, j) == 'q')  
+                        black = black + 9;
+                    if (board.getPiece(i, j) == 't')  
+                        black = black + 5;
+                    if (board.getPiece(i, j) == 'b')  
+                        black = black + 3.5;
+                    if (board.getPiece(i, j) == 'c')  
+                        black = black + 3;
+                    }
+                }
+            }
+        return (white - black);
+    }
+
+    public double kingSafety() throws BoardOutOfBoundsException {
+        double white = 0;
+        double black = 0;
+        for (int i = 0; i<8; i++) {
+            for (int j = 0; j<8; j++) {
+                if (board.getPiece(i, j) == 'K') {
+                    LinkedList<Coordinate> safeWhite = Controller.getQueenMoves(board, i, j);
+                    white = Math.sqrt(safeWhite.size());
+                }
+                if (board.getPiece(i, j) == 'k') {
+                    LinkedList<Coordinate> safeBlack = Controller.getQueenMoves(board, i, j);
+                    black = Math.sqrt(safeBlack.size());
+                }
+            }
+        }
+        return (white - black);
+    }
+}
