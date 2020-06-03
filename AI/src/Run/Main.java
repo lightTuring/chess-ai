ee3c4a787f68b7878d690e3c14f84153715ebbf3
Felipe Santos <lipe.fssantos063@gmail.com>
2020-06-03 18:24:28 -0300

    Main

---
+++ AI/src/Run/Main.java
@@ -0,0 +1,17 @@
+package Run;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        CLASSE PRINCIPAL DE EXECUÇÃO        
+
+        -> *TURNO DA MÁQUINA*
+        VERIFICA SE OS MOVIMENTOS GERADOS SÃO LEGAIS
+        ATUALIZA O ARRAY
+        CONSTROI O GRAFO
+        APLICA MINMAX
+        FAZ A JOGADA
+        -> *TURNO DO JOGADOR*
+        */
+    }
+}
