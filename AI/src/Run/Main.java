7bc4c66cfddee91da8f226d55b8f238a05647cd4
Felipe Santos <lipe.fssantos063@gmail.com>
2020-06-03 21:06:22 -0300

    atualizando com algumas observações

--- AI/src/Run/Main.java
+++ AI/src/Run/Main.java
@@ -5,14 +5,44 @@ public class Main {
         /*
         CLASSE PRINCIPAL DE EXECUÇÃO        
 
+        *PROJETO DE TESTE*
+    
+        -> *TURNO DO JOGADOR*
+        
+        FAZ A JOGADA PELO TERMINAL
+        
         -> *TURNO DA MÁQUINA*
+        
         VERIFICA SE OS MOVIMENTOS GERADOS SÃO LEGAIS
         ATUALIZA O ARRAY
         ATRIBUI OS PESOS AOS NÓS FINAIS
         CONSTROI O GRAFO
         APLICA MINMAX
         FAZ A JOGADA
+    
         -> *TURNO DO JOGADOR*
+
+
+        *PROJETO FINAL*
+
+        -> *TURNO DO JOGADOR*
+        
+        PYTHON MANDA PRO JAVA A JOGADA
+        
+        -> *TURNO DA MÁQUINA*
+        
+        VERIFICA SE OS MOVIMENTOS GERADOS SÃO LEGAIS
+        ATUALIZA O ARRAY
+        ATRIBUI OS PESOS AOS NÓS FINAIS
+        CONSTROI O GRAFO
+        APLICA MINMAX
+        FAZ A JOGADA
+        MANDA PARA O PYTHON/ARDUINO
+        
+        -> *TURNO DO JOGADOR*
+        
+                ...
+        
         */
     }
 }
