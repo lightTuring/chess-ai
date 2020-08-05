7ac3b075357e58a71f3e7d265ef37e3dcdd3358c
Felipe Santos <lipe.fssantos063@gmail.com>
2020-08-05 17:53:09 -0300

    corrigindo acesso invalido

--- AI/src/Rules/Controller.java
+++ AI/src/Rules/Controller.java
@@ -12,22 +12,22 @@ public class Controller {
 		// Se o peão for Branco:
 		if (b.getPiece(i, j) == 'P') {
 			// Se a casa na diagonal esquerda tiver alguma peça:
-			if (b.getPiece(i-1, j-1) != 'o') {
+			if (!(j-1 < 0 || j-1>=8 || i-1 < 0 || i-1>=8) && b.getPiece(i-1, j-1) != 'o') {
 				x = new Coordinate(i-1, j-1);
 				movesP.add(x);
 			}
 				// Se a casa na diagonal direita tiver alguma peça:
-			if (b.getPiece(i-1, j+1) != 'o') {
+			if (!(j+1 < 0 || j+1>=8 || i-1 < 0 || i-1>=8) && b.getPiece(i-1, j+1) != 'o') {
 				x = new Coordinate((i-1), (j+1));
 				movesP.add(x);
 			}
 			// Se a casa a frente estiver vazia:
-			if (b.getPiece(i-1, j) == 'o') {
+			if (!(i-1 < 0 || i-1>=8) && b.getPiece(i-1, j) == 'o') {
 				x = new Coordinate((i-1), (j));
 				movesP.add(x);
 			}
 			// Se no primeiro movimento do peão, a casa na frente da frente estiver vazia:
-			if (b.hasPawnMoved(i, j) == false && b.getPiece(i-2, j) == 'o') {
+			if (!(i-2 < 0 || i-2>=8) && b.hasPawnMoved(i, j) == false && b.getPiece(i-2, j) == 'o') {
 				x = new Coordinate((i-2), (j));
 				movesP.add(x);
 			}
@@ -38,16 +38,16 @@ public class Controller {
 				movesP.add(x);
 			}
 	
-			if (b.getPiece(i+1, j-1) != 'o' ) {
+			if (!(j-1 < 0 || j-1>=8 || i+1 < 0 || i+1>=8) && b.getPiece(i+1, j-1) != 'o' ) {
 				x = new Coordinate((i+1), (j-1));
 				movesP.add(x);
 			}
 	
-			if (b.getPiece(i+1, j) == 'o') {
+			if (!(i+1 < 0 || i+1>=8) && b.getPiece(i+1, j) == 'o') {
 				x = new Coordinate((i+1), (j));
 				movesP.add(x);
 			}
-			if (b.hasPawnMoved(i, j) == false && b.getPiece(i+2, j) == 'o') {
+			if (!(i+2 < 0 || i+2>=8) && b.hasPawnMoved(i, j) == false && b.getPiece(i+2, j) == 'o') {
 				x = new Coordinate((i+2), (j));
 				movesP.add(x);
 			}
