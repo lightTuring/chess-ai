52b155fda3179935af7fa81bb72e2968dda30e24
Felipe Santos <lipe.fssantos063@gmail.com>
2020-08-20 00:31:29 -0300

    tentando corrigir

--- AI/src/Rules/Controller.java
+++ AI/src/Rules/Controller.java
@@ -85,7 +85,7 @@ public class Controller {
     	Coordinate c;
 		for (int x = -1; x<=1 && x>=-1; x++) {
 			for (int y = -1; y<=1 && y>=-1; y++) {
-				if ((y != 0 || x!=0) && (i + x)<8 && (j+y)<8 && (i + x)>=0 && (j+y)>=0) {
+				if ((y != 0 || x!=0) && (i + x)<8 && (j+y)<8 && (i + x)>=0 && (j+y)>=0 && (b.getPiece(i+x,j+y) == 'o'||!b.hasSameColor(i, j, i+x, j+y))) {
 					c = new Coordinate(i + x, j+y);
 					moves.add(c);
 				}
