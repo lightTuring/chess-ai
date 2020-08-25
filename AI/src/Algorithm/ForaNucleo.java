274310b0eb3ca768e3d66690c3917612b287d069
lightTuring <cloud.losing@gmail.com>
2020-08-24 21:46:38 -0300

    Fora Núcleo!

---
+++ AI/src/Algorithm/ForaNucleo.java
@@ -0,0 +1,13 @@
+package Algorithm;
+
+import Rules.Board;
+
+public class ForaNucleo {
+    public Board board;
+    public GraphBuilder gb;
+    
+    public ForaNucleo(Board board, GraphBuilder gb) {
+        this. board = board;
+        this.gb = gb;
+    }
+}
