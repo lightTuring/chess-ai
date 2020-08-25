8c1530aff56aaeabb92ae75a053ec458edb235d6
Felipe Santos <lipe.fssantos063@gmail.com>
2020-08-24 22:05:50 -0300

    .

--- AI/src/Algorithm/ForaNucleo.java
+++ AI/src/Algorithm/ForaNucleo.java
@@ -10,4 +10,16 @@ public class ForaNucleo {
         this.board = board;
         this.gb = gb;
     }
+
+    private static void createPlaying(GraphBuilder gb){
+        //chama recursao 
+    }
+
+    public void getGlobalPlaying(){
+        for(todas as peças){
+            if(pode jogar){
+                createPlaying(new GraphBuilder());
+            }
+        }
+    }
 }
