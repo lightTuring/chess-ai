f6e24231a1af834daa7b46f35069af193c423d92
Pedro Victor de Brito Cordeiro <pedrocga1@gmail.com>
2020-11-24 17:36:21 -0300

    Revert "Substitui for-loops de preenchimento por método otimizados"
    This reverts commit acc905cc09d3f6bbefcaa193dfc3452681d9f408.

--- AI/src/Run/PvM.java
+++ AI/src/Run/PvM.java
@@ -1,88 +1,76 @@
-package Rules;
+package Run;
 
-import java.util.Arrays;
+import Rules.Bits;
+import java.util.*;
+import Algorithm.*;
+import Notation.*;
 
-//Métodos foram movidos para manipulator, deixando essa classe apenas para armazenar dados
-public class Bits {
-    public char[][] chessBoard = new char[8][8];
-    public final char[] initPosBlack = { 't', 'c', 'b', 'q', 'k', 'b', 'c', 't' };
-    public final char[] initPosWhite = { 'T', 'C', 'B', 'Q', 'K', 'B', 'C', 'T' };
-    public long board = 0L;
-    public long white = 0L;
-    public long black = 0L;
-    public long cb = 0L;
-    public long cw = 0L;
-    public long pb = 0L;
-    public long pw = 0L;
-    public long bb = 0L;
-    public long bw = 0L;
-    public long tb = 0L;
-    public long tw = 0L;
-    public long kb = 0L;
-    public long kw = 0L;
-    public long qb = 0L;
-    public long qw = 0L;
-    public boolean endOfGame = false;
-    public boolean checkmateBlack = false;
-    public boolean checkmateWhite = false;
-    public boolean turn = true;
+import Rules.Game;
+import Rules.IllegalMoveException;
+import Rules.Manipulator;
+import Rules.UnexpectedPieceException;
 
-    public Bits() {
-        for (int i = 0; i < chessBoard.length; i++) {
-            for (int j = 0; j < chessBoard[i].length; j++) {
-                chessBoard[i][j] = 'o';
+//rodar em um while
+public class PvM {
+    public static void main(String[] args) throws Exception {
+        Manipulator.init();
+        Bits bit = new Bits();
+        Scanner s = new Scanner(System.in);        
+        int depth = 5;
+
+        while (!bit.endOfGame) {
+            Manipulator.printImage(bit);
+            AlphaBeta foraNucleo = new AlphaBeta(bit);
+            System.out.println();
+            if(!bit.turn){
+                try {
+                    long time = System.currentTimeMillis();                    
+                    bit = foraNucleo.bestPlaying(0, depth, false);
+                    //System.out.println(foraNucleo.getGraph().HowManyNodes());
+                    Game g = new Game(bit);
+                    long tn = System.currentTimeMillis();
+                    System.out.println(tn - time);                    
+                }
+                catch (UnexpectedPieceException b) {
+                    System.err.println("\n*Não é peça*\n");
+                }
+                catch (CloneNotSupportedException clone) {
+                    System.err.println("\n*Clone Errado*\n");
+                }
+                catch (IllegalMoveException i) {
+                    System.err.println("\n*Movimento ilegal*\n");
+                }
+                catch(ArrayIndexOutOfBoundsException a){
+                    System.err.println("\n*Indice inválido*\n");
+                }
+                //game.setTurn(!game.getTurn());
+            }else {
+                System.out.print("Peça da posição.: ");
+                String first = s.nextLine();
+                System.out.print("Para.: ");
+                String second = s.nextLine();
+                try {
+                    Game g = new Game(bit);
+                    int sqi = Translator.NotationChessToComputer(first.charAt(0),first.charAt(1) - '0' );
+                    int sqf = Translator.NotationChessToComputer(second.charAt(0), second.charAt(1)- '0');
+                    g.move(sqi, sqf);
+                    g.allLegal();
+                    g.isCheckMate();
+                }
+                catch(StringIndexOutOfBoundsException b){
+                    System.err.println("\n*Entrada Inválida*\n");
+                }
+                catch (IllegalMoveException i) {
+                    System.err.println("\n*Movimento ilegal*\n");
+                }
             }
         }
-        
-        // Ao invés de usar for-loop, java.util já tem um método otimizado pra isso:
-        Arrays.fill(chessBoard[1], 'p');
-        Arrays.fill(chessBoard[6], 'P');
-        // initPosBlack e White são "final", então não tem problema dar assignment direto assim:
-        chessBoard[0] = initPosBlack;
-        chessBoard[7] = initPosWhite;
-
-        Manipulator.makeBoards(this);
-    } 
-
-    public Bits(char[][] board) {
-        chessBoard = board;
-        Manipulator.makeBoards(this);
-    } 
-
-    @Override
-    public Bits clone() {
-        Bits b = new Bits();
-        for (int i= 0; i<8; i++) {
-            for (int j= 0; j<8; j++) {
-                b.chessBoard[i][j] = this.chessBoard[i][j];
-            }
-        }
-        b.board = this.board;
-        b.pb = this.pb;
-        b.pw = this.pw;
-        b.bb = this.bb;
-        b.bw = this.bw;
-        b.cb = this.cb;
-        b.cw = this.cw;
-        b.tb = this.tb;
-        b.tw = this.tw;
-        b.kb = this.kb;
-        b.kw = this.kw;
-        b.qb = this.qb;
-        b.qw = this.qw;
-        b.white = this.white;
-        b.black = this.black;
-        b.checkmateBlack = this.checkmateBlack;
-        b.checkmateWhite = this.checkmateWhite;
-        b.turn = this.turn;
-        /*
-        if (this.lastMove[0] != null) {
-            b.lastMove[0] = this.lastMove[0].clone();
-        }
-        if (this.lastMove[1] != null) {
-            b.lastMove[1] = this.lastMove[1].clone();
-        }*/
-        return b;
+        Manipulator.printImage(bit);
+        System.out.println(" ");
+        System.out.println("Jogo Acabou");
+        s.close();
     }
-    
 }
+/* e2
+
+*/ 
